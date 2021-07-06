05a9c25c3f780f98f17db6520423b5692cfc183b
Jon Wilson <jonwilsoncomedy@gmail.com>
2021-07-06 00:01:52 -0500

    Adding simple auth

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { LoginButton } from '@inrupt/solid-ui-react'
+
+const authOptions = {
+  clientName: "Solid Todo App",
+}
 
 function App() {
+  const [oidcIssuer, setOidcIssuer] = useState("")
+
+  const handleChange = (event) => {
+    console.log("handlechange called ")
+    setOidcIssuer(event.target.value)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <span>
+        Login with:
+        <input 
+          className="oidc-issuer-input"
+          list="providers"
+          name="oidcIssuer"
+          onChange={(e) => handleChange(e)}
+          type="text"
+          value={oidcIssuer}
+        />
+        <datalist id="providers">
+          <option value="https://broker.pod.inrupt.com/" />
+          <option value="https://inrupt.net/" />
+        </datalist>
+      </span>
+      <LoginButton 
+        oidcIssuer={oidcIssuer}
+        redirectUrl={window.location.href}
+        authOptions={authOptions}
+      />
     </div>
   );
 }
